0ff25542fced3bbf984404d9055474531b9a3929
Renato Soares <renatosoarespro@hotmail.com>
2021-01-03 16:17:39 -0300

    saves audio from video id

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <form method="post" @submit.prevent="sendSourceUri">
+            <label for="media_audio_source_id">Source id</label>
+            <input type="text" name="media_audio_source_id" id="media_audio_source_id" v-model="form.media_audio_source_id"/>
+            <button type="submit">Send</button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            form: this.$inertia.form(
+                {
+                    '_method': 'POST',
+                    media_audio_source_id: this.media_audio_source_id,
+                },
+                {
+                    bag: "sendSourceUri",
+                    resetOnSuccess: true,
+                }
+            ),
+        };
+    },
+
+    methods: {
+        sendSourceUri() {
+            this.form.post('/media-audio', {
+                preserveScroll: true
+            });
+        },
+    },
+};
+</script>
+
+<style></style>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Str;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/media-audio', function () {
+    return Inertia\Inertia::render('Home');
+})->name('media-audio.create');
+
+Route::post('/media-audio', function (Request $request) {
+    $vid = $request->media_audio_source_id;
+
+    parse_str(
+        file_get_contents(
+            sprintf('https://www.youtube.com/get_video_info?video_id=%s', $vid)
+        ),
+        $info
+    );
+
+    $videoData = json_decode($info['player_response'], true);
+    $videoDetails = $videoData['videoDetails'];
+    $streamingData = $videoData['streamingData'];
+
+    $video_title = $videoDetails['title'];
+
+    $audio = last($streamingData['adaptiveFormats']);
+
+    file_put_contents(storage_path('app/public/' . Str::kebab($video_title) . '.mp3'), fopen($audio['url'], 'r'));
+    return redirect()->route('media-audio.create');
+})->name('media-audio.store');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia\Inertia::render('Dashboard');
