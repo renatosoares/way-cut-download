783b5ffc01230b3d2b3615e110d36013814642f6
Renato Soares <renatosoarespro@hotmail.com>
2021-01-16 06:21:55 -0300

    re-set project

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,43 +13,6 @@ use Illuminate\Support\Str;
 |
 */
 
-Route::get('/media-audio', function () {
-    $files = collect(Storage::files())
-        ->filter(fn ($file) => $file !== '.gitignore')
-        ->values();
-
-    return Inertia\Inertia::render('Home', compact(
-        'files',
-    ));
-})->name('media-audio.create');
-
-Route::post('/media-audio', function (Request $request) {
-    $vid = $request->media_audio_source_id;
-
-    parse_str(
-        file_get_contents(
-            sprintf('https://www.youtube.com/get_video_info?video_id=%s', $vid)
-        ),
-        $info
-    );
-
-    $videoData = json_decode($info['player_response'], true);
-    $videoDetails = $videoData['videoDetails'];
-    $streamingData = $videoData['streamingData'];
-
-    $video_title = $videoDetails['title'];
-
-    $audio = last($streamingData['adaptiveFormats']);
-
-    // file_put_contents(storage_path('app/public/' . Str::slug($video_title, '-') . '.mp3'), fopen($audio['url'], 'r'));
-    Storage::put(Str::slug($video_title, '-') . '.txt', $videoDetails['title']);
-
-    return Redirect::back()->with('success', 'File saved.');
-})->name('media-audio.store');
-
-Route::delete('/media-audio/{file_path}', function (Request $request) {
-
-    Storage::delete($request->file_path);
-
-    return Redirect::back()->with('success', 'File deleted.');
-})->name('media-audio.destroy');
+Route::get('/', function () {
+    return view('welcome');
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-mix.disableNotifications();
 
 /*
  |--------------------------------------------------------------------------
@@ -14,7 +13,5 @@ mix.disableNotifications();
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ])
-    .webpackConfig(require('./webpack.config'));
+        //
+    ]);
